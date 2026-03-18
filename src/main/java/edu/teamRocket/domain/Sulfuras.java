8c773898fe1ccc4dbb73a9fceed87d429641f8c5
|solrac| <gabrieldiacich@gmail.com>
2026-03-18 13:46:27 +0100

    feat(Sulfuras): diseñar subclase Sulfuras en base a los tests

---
+++ src/main/java/edu/teamRocket/domain/Sulfuras.java
@@ -0,0 +1,11 @@
+package edu.teamRocket.domain;
+
+public class Sulfuras extends NormalItem {
+
+    public Sulfuras(String name, int sell_In, int quality) {
+        super(name, sell_In, quality);
+    }
+
+    @Override
+    public void updateQuality() {}
+}
